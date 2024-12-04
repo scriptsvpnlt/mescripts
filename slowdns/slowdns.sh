6f2dc65abf7e0835bb7e36adad9a7354f17d3fe5
scriptsvpnlt <autoscriptfinishing@gmail.com>
2024-12-05 01:18:03 +0700

    Add files via upload

---
+++ slowdns/slowdns.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# ===============================================
+sudo apt install squid -y
+#setting IPtables
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+netfilter-persistent save
+netfilter-persistent reload
+
+
+
+#delete directory
+rm -rf /root/nsdomain
+rm nsdomain
+
+#input nameserver manual to cloudflare
+touch /etc/xray/nsdomain
+
+Host=inject.cloud
+sub=ns.`(</dev/urandom tr -dc a-z0-9 | head -c5)`
+#sub=ns.`</dev/urandom tr -dc x-z0-9 | head -c4`
+SUB_DOMAIN=${sub}.inject.cloud
+NS_DOMAIN=${SUB_DOMAIN}
+echo $NS_DOMAIN > /etc/xray/nsdomain
+
+#nameserver=$(cat /root/nsdomain)
+nameserver=$(cat /etc/xray/nsdomain)
+apt update -y
+apt install -y python3 python3-dnslib net-tools
+apt install dnsutils -y
+#apt install golang -y
+apt install git -y
+apt install curl -y
+apt install wget -y
+apt install screen -y
+apt install cron -y
+apt install iptables -y
+apt install -y git screen whois dropbear wget
+apt install -y sudo gnutls-bin
+apt install -y dos2unix debconf-utils
+service cron reload
+service cron restart
+
+
+
+
+#konfigurasi slowdns
+rm -rf /etc/slowdns
+mkdir -m 777 /etc/slowdns
+wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/rzlftwaa-cloud/scriptsme/main/slowdns/server.key"
+wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/rzlftwaa-cloud/scriptsme/main/slowdns/server.pub"
+wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/rzlftwaa-cloud/scriptsme/main/slowdns/sldns-server"
+wget -q -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/rzlftwaa-cloud/scriptsme/main/slowdns/sldns-client"
+cd
+chmod +x /etc/slowdns/server.key
+chmod +x /etc/slowdns/server.pub
+chmod +x /etc/slowdns/sldns-server
+chmod +x /etc/slowdns/sldns-client
+
+cd
+
+#install client-sldns.service
+cat > /etc/systemd/system/client-sldns.service << END
+[Unit]
+Description=Client SlowDNS By CyberVPN
+Documentation=https://www.xnxx.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/etc/slowdns/sldns-client -udp 8.8.8.8:53 --pubkey-file /etc/slowdns/server.pub $nameserver 127.0.0.1:58080
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+cd
+#install server-sldns.service
+cat > /etc/systemd/system/server-sldns.service << END
+[Unit]
+Description=Server SlowDNS By Cybervpn
+Documentation=https://xhamster.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $nameserver 127.0.0.1:22
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+#permission service slowdns
+cd
+chmod +x /etc/systemd/system/client-sldns.service
+
+chmod +x /etc/systemd/system/server-sldns.service
+pkill sldns-server
+pkill sldns-client
+
+systemctl daemon-reload
+systemctl stop client-sldns
+systemctl stop server-sldns
+
+systemctl enable client-sldns
+systemctl enable server-sldns
+
+systemctl start client-sldns
+systemctl start server-sldns
+
+systemctl restart client-sldns
+systemctl restart server-sldns
